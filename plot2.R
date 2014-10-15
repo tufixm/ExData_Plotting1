0288029bb35370ec8d1296964ebbd3d165bd9df9
tufixm <tufixm@gmail.com>
2014-10-15 02:14:35 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+# this may be redundant, if the data is already loaded
+# the file should be unzipped
+# the text file should be placed in the working directory
+
+# read the raw data
+house.power.raw = read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+house.power.raw$Date = as.Date(house.power.raw$Date, format="%d/%m/%Y")
+
+# filter the data for the 2 needed days
+pwr.data = house.power.raw[house.power.raw$Date == as.Date("2007-02-01") | house.power.raw$Date == as.Date("2007-02-02"),]
+
+# merge the Date and Time columns into a DateTime common column
+pwr.data$DateTime = strptime(paste(pwr.data$Date, pwr.data$Time), format="%F %T")
+
+# plot the 2nd figure
+png("plot2.png")
+plot(pwr.data$DateTime, pwr.data$Global_active_power, type="l", xlab="", ylab="Global active power (kilowatts)")
+dev.off()
